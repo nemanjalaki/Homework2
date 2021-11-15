09ae2ace946316b81f77f64f16d88f9e9d8b69c2
nemanjalaki <sone.lakicevic@gmail.com>
2021-11-15 22:50:06 +0100

    all operations working

--- Palindrom/dom.js
+++ Palindrom/dom.js
@@ -1,6 +1,7 @@
 // listeners
 var form = document.getElementById('oneCellForm');
 var itemList = document.getElementById("cellContainer");
+var li = itemList.getElementsByTagName("li");
 var formInput = document.getElementById('firstForm');
 
 
@@ -9,8 +10,8 @@ var formInput = document.getElementById('firstForm');
 form.addEventListener("submit", addOneCell);
 formInput.addEventListener("submit", createCells);
 itemList.addEventListener("click", removeCell);
-itemList.addEventListener("keyup", inputLetter);
-// itemList.addEventListener("onchange", updateLetter);
+itemList.addEventListener("input", inputLetter);
+// itemList.addEventListener("select", updateLetter);
 
 //message 
 const message = document.querySelector('.message');
@@ -22,7 +23,7 @@ function createCells(e){
     e.preventDefault();
     var number = document.getElementById("inputFirst").value;
     inputNumber = parseInt(number);
-    console.log(inputNumber);
+    // console.log(inputNumber);
     for(i=0; i< inputNumber ; i++)
     addOneCell(e);
 }
@@ -31,12 +32,20 @@ function addOneCell(e){
     e.preventDefault();
     var li = document.createElement("li");
     li.className="cell";
+    li.id="cell";
     //create element input
     var newCell = document.createElement("input");
+    //add type to input element
+    newCell.type= "text";
     //add class to input element
     newCell.className= "input";
+    //add id to input element
+    // newCell.setAttribute("id","input");
+    newCell.id = "input";
     //add maxsize
     newCell.maxLength="1";
+     //add size
+     newCell.size="1";
     // add delete button 
     var delBtn=document.createElement("button");
     // add class to button 
@@ -52,42 +61,41 @@ function addOneCell(e){
 }
 // delete cell
 function removeCell(e){
+    e.preventDefault();
+    var string = "";
     if(e.target.classList.contains('removeCell')){
-        if (confirm('Are you sure?')){
-          var li = e.target.parentElement;
-          itemList.removeChild(li);
-     
-          checkPalindrome();
-        }
+        // if (confirm('Are you sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+        items = itemList.getElementsByTagName("li");
+        Array.from(items).forEach(function(item){
+            var itemLetter = item.firstChild.value;
+            string += itemLetter;
+            checkPalindrome(string);
+        });
     }
 }
 // check letter 
-var string = "";
-function inputLetter(e){
-    
-    var checkLetter = /^[A-Za-z ]*$/;
-    var letter = e.target.value;
-    if(letter.match(checkLetter)){
-          string += letter;
-        // console.log(string);
-        checkPalindrome(string);
-    }else{
-        alert("Input char must be A-Z or a-z and space");
-        e.target.value = '';
-    }
-    return string;
-    // console.log(string);
-}
 
-// console.log(string);
-//updateLetter
-// function updateLetter(e){
-//     var old = document.getElementById('input').value;
-//     old = e.target.value;
-//     // var element = e.target.value;
-//     // inputLetter();
-//     console.log(old);
-// }
+function inputLetter(e){
+    var checkLetter = /^[A-Za-z ]*$/;
+    var string = "";
+    // vrati mi poziciju ako je prazna upisuj
+    if (e.target.value.match(checkLetter)){
+        var letter = e.target.value;
+        console.log(letter);
+        items = itemList.getElementsByTagName("li");
+        Array.from(items).forEach(function(item){
+            var itemLetter = item.firstChild.value;
+            string += itemLetter;
+            console.log(string);
+            checkPalindrome(string);
+        })
+        }else{
+            alert("Input char must be A-Z or a-z and space");
+            e.target.value = "";
+}
+}
 
 // check palindrome 
 function checkPalindrome(splitString){
@@ -95,16 +103,26 @@ function checkPalindrome(splitString){
     splitString = splitString.replace(/\s+|\s+$/gm,'');
     // split to return array
     splitStringToArray = splitString.split("");
+    // remove space
+    // splitStringToArray = splitStringToArray.filter(function(str) {
+    //     return /\S/.test(str);
+    // });
+    // console.log(splitStringToArray);
     // reverse() to reverse new created array
     var reverseArray = splitStringToArray.reverse();
     // join array
     var joinArray = reverseArray.join("");
 
-    if(splitString == joinArray){
+    if(splitString === joinArray){
+        // console.log(splitString);
+        // console.log(joinArray);
         message.innerHTML = palindrome;
     }
     else{
+        // console.log(splitString);
+        // console.log(joinArray);
         message.innerHTML = notPalindrome;
     }
-
+    
 }
+
